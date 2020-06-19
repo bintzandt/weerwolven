3167ef0abe8c08629836b3074b19da9842a94e40
Dion <d@sgep.nl>
2020-06-19 10:19:03 +0200

    Refactored to seperate file

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,95 +1,13 @@
 import * as React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { fetchAPI } from './Api';
-import DatePicker from 'react-datepicker'
+import { CreateGame } from './CreateGame'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
-
-interface IState {
-  roles: Map<string, boolean>
-  date: Date
-}
-
-interface IProps {
-}
-
-class CreateGame extends React.Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props)
-    this.state = {roles: new Map<string, boolean>(), date: new Date()}
-  }
-
-  async fetchRoles() {
-    const roles = new Map<string, boolean>()
-    await fetchAPI('/game/roles')
-            .then(ls => ls.forEach((el: string) => {
-              roles.set(el, false)
-            }))
-    this.setState({
-      roles: roles
-    })
-  }
-
-  componentDidMount() {
-    this.fetchRoles()
-  }
-
-  submit(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-    e.preventDefault();
-    console.log(this.state)
-  }
-
-  onChangeListen(type: string, value: any) {
-    const roles = this.state.roles
-    var date = this.state.date
-
-    switch (type) {
-      case "role":
-        roles.set(value, this.state.roles.get(value) ? false : true)
-        break;
-      case "date":
-        date = value
-      default:
-        break;
-    }
-    this.setState({roles: roles, date: date})
-  }
-
-  render() {
-    return (
-      <form>
-          <h1>Create game</h1>
-          <DatePicker selected={this.state.date} onChange={date => this.onChangeListen("date", date)}  placeholderText="MM/DD/YYYY"/>
-          <ul>
-              { [...this.state.roles.keys()].map((value) => (
-                  <Checkbox key={value} label={value} isSelected={this.state.roles.get(value)} onChangeListener={this.onChangeListen.bind(this)} />
-              ))}
-          </ul>
-          <button type="submit" onClick={this.submit.bind(this)}>Submit!</button>
-      </form>
-      )
-  }
-}
-
-const Checkbox = ({ label, isSelected, onChangeListener } : any) => (
-  <div className="form-check">
-    <label>
-      <input
-        type="checkbox"
-        name={label}
-        onChange={() => onChangeListener("role", label)}
-        defaultChecked={isSelected}
-        className="form-check-input"
-      />
-      {label}
-    </label>
-  </div>
-);
 
 function MyGames() {
     return <h1>My games</h1>
